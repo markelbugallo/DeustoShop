763782d755a85152e67387ba1e11ad3c39b7b469
Markel Bugallo <markel.bugallo@opendeusto.es>
2025-05-23 20:52:41 +0200

    He creado las funciones para leer los datos de CSV que faltaban

--- DeustoShop/DeustoShopCpp/BD/Bd.h
+++ DeustoShop/DeustoShopCpp/BD/Bd.h
@@ -1,8 +1,11 @@
 #ifndef BD_H
 #define BD_H
 #include "../Clases/Almacen.h"
-#include "../Clases/Producto.h"
 #include "../Clases/Pedido.h"
+#include "../Clases/Producto.h"
+#include "../Clases/Proveedor.h"
+#include "../Clases/Subscripcion.h"
+#include "../Clases/Usuario.h"
 #include <string>
 #include <vector>
 using namespace std;
@@ -10,4 +13,7 @@ using namespace std;
 vector<Almacen> cargarAlmacenesCSV(const string &fichero);
 vector<Pedido> cargarPedidosCSV(const string &fichero);
 vector<Producto> cargarProductosCSV(const string &fichero);
+vector<Proveedor> cargarProveedoresCSV(const string &fichero);
+vector<Subscripcion> cargarSubscripcionesCSV(const string &fichero);
+vector<Usuario> cargarUsuariosCSV(const string &fichero);
 #endif // BD_H
